662c44a053c9975b8bf832631fd4532f83fc8128
ambertan <ambertjn@gmail.com>
2024-06-17 16:56:40 +0800

    implement date picker

--- app/screens/habit/index.jsx
+++ app/screens/habit/index.jsx
@@ -1,23 +1,44 @@
 import React from 'react';
 import { useState } from 'react';
 import { View, Text, ScrollView, TouchableWithoutFeedback } from 'react-native';
-import { KeyboardAvoidingView, Platform, Keyboard } from 'react-native'
+import { KeyboardAvoidingView, Platform, Keyboard, Button } from 'react-native'
 import { StyleSheet, SafeAreaView} from 'react-native';
 import tw from 'twrnc';
 import { useNavigation } from 'expo-router';
 import ButtonTemplate from '../../components/buttonTemplate';
 import TextInputTemplate from '../../components/textInputTemplate';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import RNDateTimePicker from '@react-native-community/datetimepicker';
+import { TouchableOpacity } from 'react-native';
 
 export default function Index() {
 
   const [name, setName] = useState('')
   const [frequency, setFrequency] = useState('')
   const [period, setPeriod] = useState('')
+  const [date, setDate] = useState(new Date())
+  const [mode, setMode] = useState('date')
+  const [show, setShow] = useState(false)
   const navigation = useNavigation()
 
   const handleCreation = () => {
     navigation.navigate('screens/calendar/index');
   }
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setShow(false);
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
 
   return (
 
@@ -53,10 +74,28 @@ export default function Index() {
 
               <TextInputTemplate 
                 type='2'
-                text= 'Enter the period your habit is active for'
+                text= 'Enter the period of your new habit'
                 setText = {text => setPeriod(text)}
                 value={period}
               />
+
+              <View style={tw `flex-1 justify-center items-center mb-2`}>
+                <ButtonTemplate 
+                  type = 'add'
+                  size = 'med'
+                  text = '+ ADD SCHEDULED DATE'
+                  onPress = {showDatepicker}
+                />
+                <Text>Date selected: {date.toLocaleDateString()}</Text>
+                {show && (
+                  <DateTimePicker
+                    testID="dateTimePicker"
+                    value={date}
+                    mode={mode}
+                    onChange={onChange}
+                  />
+                )}
+              </View>
         
               <ButtonTemplate
                 type = 'green' 
