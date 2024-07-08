d0da058c59bf6084a8cefafc543fae4a4f55c9c9
varshini <nagam.varshini@gmail.com>
2024-07-09 00:01:32 +0800

    add post button in fav page

--- app/components/textInputTemplate.tsx
+++ app/components/textInputTemplate.tsx
@@ -2,7 +2,7 @@ import { TouchableOpacity, Text, TextInput } from "react-native";
 import tw from 'twrnc';
 
 type TextProps = {
-    type: "1" | "2";
+    type: "1" | "2" | "3" ;
     text: string;
     setText: () => void;
     value: string;
@@ -10,15 +10,18 @@ type TextProps = {
 
 const TextInputTemplate = ({ type, text, setText, value }: TextProps) => {
     const secure = type === "1" ? true : false;
+    const lines = type === "3" ? true : false;
+    const styling = type === "3" ? "w-full h-15 p-2 bg-white rounded-lg mb-3" : "w-4/5 p-4 bg-white rounded-lg mb-3";
     return (
         
         <TextInput
-            style={tw`w-4/5 p-4 bg-white rounded-lg mb-3 border border-gray-400`}
+            style={tw`${styling} border border-gray-400`}
             placeholder={text}
             secureTextEntry={secure}
             onChangeText={setText}
             autoCapitalize="none"
             value={value}
+            multiline={lines}
         />
     );
 };
