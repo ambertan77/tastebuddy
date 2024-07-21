9840f1aa29c727cb687ccb52aa54a09f819d8cfe
ambertan <ambertjn@gmail.com>
2024-07-22 00:12:13 +0800

    add back buttons to followers and following page

---
+++ app/screens/followers/components/backButton.jsx
@@ -0,0 +1,12 @@
+import { TouchableOpacity, Text } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import tw from 'twrnc';
+import { useNavigation  } from "@react-navigation/native";
+
+export default function backButton ( {onPress} ) {
+    return (
+        <TouchableOpacity style={tw `items-start ml-3 mt-16`} onPress={onPress} testID='goBack'>
+            <Icon name="chevron-left" size="20" color="white" />
+        </TouchableOpacity>
+    );
+};
--- app/screens/followers/components/header.jsx
+++ app/screens/followers/components/header.jsx
@@ -1,10 +1,19 @@
 import { View, Text } from "react-native";
 import tw from 'twrnc';
+import BackButton from './backButton';
+import { useNavigation } from "@react-navigation/native";
 
 const Header = () => {
+    const navigation = useNavigation()
+
+    const handleBackButton = () => {
+        navigation.navigate("screens/profile/index");
+    }
+
     return (
-        <View style={tw`h-25 bg-green-700`}>
-            <Text style={tw`text-white text-xl mt-15 text-center font-bold`}>
+        <View style={tw `flex-row px-1 h-25 bg-green-700`}>
+            <BackButton onPress={handleBackButton} testId='backButton' />
+            <Text style={tw`text-white text-xl mt-15 text-center font-bold mb-2 ml-31`}>
                 followers
             </Text>
         </View>
